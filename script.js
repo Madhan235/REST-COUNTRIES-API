14e6d82832aa2a6082765e8dd5cca88c57b7b7ea
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-02-14 12:28:20 +0530

    weather added

--- script.js
+++ script.js
@@ -5,58 +5,61 @@ row.classList.add('row','m-3');
 container.append(row);
 
 var res=fetch("https://restcountries.com/v2/all");
-res.then((data)=>data.json())
-.then((data1)=>foo(data1))
+res.then((data)=>data.json()).then((data1)=>foo(data1))
 .catch((error)=>console.log(error));
 
-function foo(data1){
-    // console.log(data1);
-    for(var i=0;i<data1.length;i++){
-       row.innerHTML += 
-       `<div class="col-md-4">
-       <div class="card border-primary mb-3" style="width: 18rem;">
-       <img src="${data1[i].flag}" class="card-img-top" alt="country image">
-       <div class="card-body text-primary">
-         <h5 class="card-title"> Name : ${data1[i].name}</h5>
-         <p class="card-text"> Capital : ${data1[i].capital}</p>
-         <p class="card-text"> Population : ${data1[i].population}</p>
-         <button class="btn btn-primary" onclick="weatherdata()">Click for Weather</button>
-         <p class="card-text" id=temp></p>
-       </div>
-     </div>
-     </div>
-     `
-        }
-        document.body.append(container);
-}
+
+
+function foo(data1) {
+  var container = document.createElement('div');
+  container.className = "container";
+  document.body.append(container);  
+
+  for (var i = 0; i < data1.length; i++) {
+    if (i % 3 === 0) {
+      var row = document.createElement('div');
+      row.classList.add('row', 'm-3');
+      container.appendChild(row);   
+    }
  
+    var col = document.createElement('div');
+    col.classList.add('col-lg-4', 'col-md-6', 'col-sm-12', 'mb-3'); 
+    col.innerHTML =
+        `<div class="card border-primary mb-3" style="width: 18rem;">
+            <img src="${data1[i].flag}" class="card-img-top" alt="country image">
+            <div class="card-body text-primary">
+                <h5 class="card-title"> Name : ${data1[i].name}</h5>
+                <p class="card-text"> Capital : ${data1[i].capital}</p>
+                <p class="card-text"> Population : ${data1[i].population}</p>
+                <p class="tempData${i}">Weather : click ⬇️</p>
+                <button class="btn btn-primary" onclick="weatherdata(${data1[i].latlng[0]}, ${data1[i].latlng[1]}, ${i})">Click for Weather</button>
+            </div>
+        </div>`;
 
-async function getdata(){
-  var res=await fetch("https://restcountries.com/v2/all");
-  var res1= await res.json();
-
-  for(var i=0;i<res1.length;i++){
-      try {
-          // console.log(`Latitude:${res1[i].latlng[0]} Longitude:${res1[i].latlng[1]}`);
-          weatherdata(res1[i].latlng[0],res1[i].latlng[1]);
-      } 
- catch (error) {
-  console.log(error);
-}
-}
-}
-
-async function weatherdata(lat,lon){
-try {
-  if(lon===undefined) throw new Error("Invalid Coordinates");
-  // console.log(lat,lon);
-  let res2=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=fa4973b1fa1ca717811b9566c55321ec`);
-  let res3=await res2.json();
-  console.log(`${res3.main.temp}`);
-} catch (error) {
- console.log(error) 
+    row.appendChild(col);  
+  }
 }
 
+async function weatherdata(lat, lon, index) {
+  try {
+    let apiKey = `8f8ec224fbb6b909071b93048675a770`;
+    if (lon === undefined) throw new Error("Invalid Coordinates");
+    let res2 = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`);
+    let res3 = await res2.json();
+    let temperature = res3?.main.temp;
+    let tempMin = res3?.main.temp_min;
+    let tempMax = res3?.main.temp_max;
+    let feelsLike = res3?.main.feels_like;
+    
+    var tempData = document.querySelector(`.tempData${index}`);
+    tempData.innerHTML = `
+      <p>Temperature: ${temperature} °F</p>
+      <p>Feels Like: ${feelsLike} °F</p>
+      <p>Max Temperature: ${tempMax} °F</p>
+      <p>Min Temperature: ${tempMin} °F</p>
+    `;
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-getdata();
+  
